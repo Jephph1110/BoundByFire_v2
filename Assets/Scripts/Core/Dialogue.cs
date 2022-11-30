6b0124b8397ff290fbf3278a17debda1d650b058
Jonathan <28bbyjonathanchoi@gmail.com>
2022-11-30 02:53:02 -0800

    Dialogue Text added Dialogue Container Styling Adjusted
    Dialogue added to dialogue script and styling adjusted to fit with our fonts.

--- Assets/Scripts/Core/Dialogue.cs
+++ Assets/Scripts/Core/Dialogue.cs
@@ -57,5 +57,78 @@ public class Dialogue : MonoBehaviour
             "I am a test dialogue",
             "That's all I have to say"
         });
+
+        //TOWN DIALOGUE---------------------------------------------------------------------------------
+        //TavernNPC1 Dialogue
+        dialogue.Add("TownNPC1", new string[] {
+            "Oy, what are ya looking for here half-beast? I ain't go nothing for ya.",
+            "Oh, you're just passing by?",
+            "Then buzz of will ya? You're ruinin' my vibe."
+        });
+
+
+        //TAVERN DIALOGUE ---------------------------------------------------------------------------------
+        //TavernNPC1 Dialogue
+        dialogue.Add("TavernNPC1", new string[] {
+            "Oy, what are ya looking for here half-beast? I ain't go nothing for ya.",
+            "Oh, you're just passing by?",
+            "Then buzz of will ya? You're ruinin' my vibe."
+        });
+
+        //TavernNPC2 Dialogue
+        dialogue.Add("TavernNPC2", new string[] {
+            "You ever tried the tavern's hot scotch?",
+            "It's the best drink you can get in Slatia. You should try it out sometimes."
+        });
+
+        //TavernNPC3 Dialogue
+        dialogue.Add("TavernNPC3", new string[] {
+            "I'm starving..... haven't had anything since leaving Evenborough.",
+            "..... oh, you were listening? Well.... this is awkward."
+        });
+
+        //TheBoss Dialogue
+        dialogue.Add("TheBoss", new string[] {
+            "Back again so soon? I don't got any quests for you right now I'm afraid.",
+            "What else can I do for you?",
+            "Quests? You trying to cut me out kid?", 
+            "Well you can find more people SIMILAR to me throughout the lands here and there looking for help.",
+            "They'll usually be willing to pay a pretty penny for a good merc like yourself.",
+            "Can't find many in Slatia, but there's some good half-beast vendors you'll be able to purchase items from with your earned gold."
+        });
+
+        dialogue.Add("TheBossChoice1", new string[] {
+            "",
+            "Nothing for now",
+        });
+
+        dialogue.Add("TheBossChoice2", new string[] {
+            "",
+            "Where can I find Quests?"
+        });
+
+        //Tavern Book Shelf Dialogue
+        dialogue.Add("TavernShelf", new string[] {
+            "It's a musty old bookshelf with a few grimy books.",
+            "Would you like to read one of them?",
+            "TITLE: THE GREAT FIRE OF SLATIA",
+            "'Not much is know about where the fires that lit up the town square of Slatia came from.'",
+            "'Although not many were injured in the incident, this proved the dangers of magic that could come from half-beasts.'",
+            "'The towns royal guard went on record to say the following after the arrest of the suspected half-beast perpertrators:'",
+            "'Fear not citizens of the Slatia, the hidden evil of the half-beasts shall not go unpunished. We shall make it an effort to ensure such an event never occurs again.'",
+            "'Restrictions shall be placed on any of those who choose to align and help those that practice such witchcraft in the future. This is a warning to everyone.'",
+            "The rest of the document is torn off and is illegible...."
+        });
+
+        dialogue.Add("TavernShelfChoice1", new string[] {
+            "",
+            "Not now",
+        });
+
+        dialogue.Add("TavernShelfChoice2", new string[] {
+            "",
+            "Fine, I'll read it"
+        });
     }
 }
+
