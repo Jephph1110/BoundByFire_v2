3ef044d76af58b1c2f2e3a2fe9ded6694e8cf6a9
vbobus <48785732+vbobus@users.noreply.github.com>
2022-11-30 05:42:39 -0800

    Deidre, NPCS 1/2, prefabs + Dialogue system integrated to first Scene

--- Assets/Scripts/Core/Dialogue.cs
+++ Assets/Scripts/Core/Dialogue.cs
@@ -61,16 +61,75 @@ public class Dialogue : MonoBehaviour
         //TOWN DIALOGUE---------------------------------------------------------------------------------
         //TavernNPC1 Dialogue
         dialogue.Add("TownNPC1", new string[] {
-            "Oy, what are ya looking for here half-beast? I ain't go nothing for ya.",
-            "Oh, you're just passing by?",
-            "Then buzz of will ya? You're ruinin' my vibe."
+            "A half-beast in Slatia? I haven't seen your kind in a while now. Say.....",
+            "Can you do the fire thing?",
+            "No? Well that's a bummer."
+        });
+
+        dialogue.Add("TownNPC2", new string[] {
+            "You know there's a lot outside Slatia right?",
+            "Evenborough Woods, the Stlagmites Shrine, and the Peaksie Canyon are some of my favourite places to visit.",
+            "Maybe you can find more of your kind there right? Ha, ha, ha..."
+        });
+
+        dialogue.Add("TownNPC3", new string[] {
+            "I'm telling you, I saw some crazy swamp things just outside the town!",
+            "Ahh you never believe me do you? They were big, they were scary, and they were.....",
+            "Uh, sorry buddy. Don't wanna be that guy, but we're kind of in the middle of a conversation here."
+        });
+
+
+        dialogue.Add("Mayor", new string[] {
+            "Hey you, half-beast! There's something going on in the outskirts to the right, you gotta help out!",
+            "Now's not the time to give me faces! Now is the time to MOVE!"
+        });
+
+        //INSIDE HOUSE DIALOGUE---------------------------------------------------------------------------------
+        dialogue.Add("HouseNPC1", new string[] {
+            "You know some people in Evenborough Woods lives in trees right? Crazy. I could never do that.",
+            "If you ask me, it's a lot cozier to be in a nice warm house. Do you know what's that like?"
+        });
+
+        dialogue.Add("HouseNPC2", new string[] {
+            "Humans, half-beasts, witches, ya da, ya da, ya da.",
+            "Hear things for too long and they start to lose their meaning. It's like a broken.....",
+            "Ah, I'll think of a name when I meet up with my buddies to play our music at the tavern"
+        });
+
+        dialogue.Add("HouseShelf1", new string[] {
+            "It's a shelf filled with a neat collection of books all about cooking.",
+            "Would you like to read one of them?",
+            "TITLE: SLATIA'S FINEST DISHES",
+            "'One of the most renown delicacies in all of Slatia is the Minced Meat Sherbert Pie.",
+            "'Layered only with the finest taters from the village of Octone, they're mixed well with delightful lamb and split peas'",
+            "'Paired with a hot scotch, this is one meal you'll never forget.",
+            "It's the end of the excerpt."
+        });
+
+        dialogue.Add("HouseShelf1Choice1", new string[] {
+            "",
+            "Not now",
+        });
+
+        dialogue.Add("HouseShelf1Choice2", new string[] {
+            "",
+            "Fine, I'll read it"
+        });
+
+        dialogue.Add("HouseBoxes1", new string[] {
+            "A tidy collection of boxes are stacked on top of each other. One of them reads 'Octone Potatoes'."
+        });
+
+        dialogue.Add("HouseBoxes2", new string[] {
+            "Empty boxes are stacked up on top of each other. One of them has writing that reads 'Peaksie....'.",
+            "The rest of the text is too faded to make out."
         });
 
 
         //TAVERN DIALOGUE ---------------------------------------------------------------------------------
         //TavernNPC1 Dialogue
         dialogue.Add("TavernNPC1", new string[] {
-            "Oy, what are ya looking for here half-beast? I ain't go nothing for ya.",
+            "Oy, what are ya looking for here half-beast? I ain't got nothing for ya.",
             "Oh, you're just passing by?",
             "Then buzz of will ya? You're ruinin' my vibe."
         });
@@ -80,18 +139,20 @@ public class Dialogue : MonoBehaviour
             "You ever tried the tavern's hot scotch?",
             "It's the best drink you can get in Slatia. You should try it out sometimes."
         });
+        
 
         //TavernNPC3 Dialogue
         dialogue.Add("TavernNPC3", new string[] {
             "I'm starving..... haven't had anything since leaving Evenborough.",
-            "..... oh, you were listening? Well.... this is awkward."
+            "..... oh, you were listening? Well.... this is awkward.",
+            "Um.... don't mind me. Seriously, look away"
         });
 
         //TheBoss Dialogue
         dialogue.Add("TheBoss", new string[] {
             "Back again so soon? I don't got any quests for you right now I'm afraid.",
             "What else can I do for you?",
-            "Quests? You trying to cut me out kid?", 
+            "Quests? Ha, ha, you trying to cut me out kid?", 
             "Well you can find more people SIMILAR to me throughout the lands here and there looking for help.",
             "They'll usually be willing to pay a pretty penny for a good merc like yourself.",
             "Can't find many in Slatia, but there's some good half-beast vendors you'll be able to purchase items from with your earned gold."
@@ -129,6 +190,7 @@ public class Dialogue : MonoBehaviour
             "",
             "Fine, I'll read it"
         });
+        
     }
 }
 
